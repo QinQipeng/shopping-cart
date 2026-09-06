272a38c241a59c90b461de4639486ca826f79c66
QinQipeng <126496939+QinQipeng@users.noreply.github.com>
2026-09-06 23:39:11 +0800

    Cleaning up the boilerplate assets

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import '/src/css/index.css'
+import ShoppingCart from './ShoppingCart'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ShoppingCart />
   </StrictMode>,
 )
