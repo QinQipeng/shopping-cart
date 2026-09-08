8010f0e12ab1e75d682dc0bfe11da443d95176c2
QinQipeng <qipeng.qin@connect.polyu.hk>
2026-09-08 10:11:35 +0800

    Home Page Content Filled

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,38 @@
 import { layout } from "/src/css/home.module.css"
 
+import logo from "/src/assets/logos/wolfy-cafe.svg"
+import logoRev from "/src/assets/logos/wolfy-cafe-rev.svg"
+import addressMap from "/src/assets/wolfyCafe-google-map.png"
+import retail from "/src/assets/wolfy-cafe-retail.png"
+
+import products from "../lib/products";
+
+
+const DISPLAY_NUM = 1
+const CONTACT_INFO = [
+  {
+    title: "Location",
+    data: "14A Cameron Road, \nTsim Sha Tsui, Kowloon, \nHong Kong"
+  },
+  {
+    title: "Working Hours",
+    data: "09:30~20:00",
+  },
+  {
+    title: "Tel",
+    data: "+852 21100226"
+  },
+  {
+    title: "Email",
+    data: "WolfyCafe@gmail.com"
+  }
+]
+
 export default function HomePage() {
   return (
     <div className={layout}>
       <section className="hero">
-        <div className="info">
+        <div className="advertize">
           <h1 className="title">Start Your Day with Wolfy Café!</h1>
           <p className="subtext">With a world champion level in fermentation and roasting techniques at the helm, our freshly brewed coffee is to elevate your day with the highest quality experience!</p>
         </div>
@@ -13,18 +41,40 @@ export default function HomePage() {
       <section className="recommendation">
         <h2>Try Our Hot Sellers of the Season!</h2>
         <div className="products">
-          
+          {
+            products.filter((_, index) => index < DISPLAY_NUM).map((product) => {
+              return (
+                <div key={product.name} className="card">
+                  <img src={product.src} alt={product.name} />
+                  <div className="prod-info">
+                    <h4>{product.name}</h4>
+                    {`${product.unit}${product.price.toFixed(2)}`}
+                  </div>
+                </div>
+              )
+            })
+          }
         </div>
       </section>
       <section className="contact">
         <div className="logo">
-          <img src="/src/assets/wolfy-cafe-rev.svg" alt="" />
+          <img src={logoRev} alt="" />
           <h3>WOLFY CAFÉ</h3>
         </div>
         <div className="contact-info">
           <ul>
-            
+            {
+              (CONTACT_INFO.map((contact) => {
+                return (
+                  <li key={contact.title}>{contact.title}: <span className="attr">{contact.data}</span> </li>
+                )
+              }))
+            }
           </ul>
+        </div>
+        <div className="address-images">
+          <img src={retail} alt="cafe retail display" />
+          <img src={addressMap} alt="cafe google map" />
         </div>
       </section>
       <footer className="footer">Copyright Wolfy Café © 2026</footer>
---
+++ src/lib/products.js
@@ -0,0 +1,33 @@
+import Americano    from "/src/assets/products/Americano.jpg"
+import Cappuccino   from "/src/assets/products/Cappuccino.jpg"
+import Latte        from "/src/assets/products/Latte.jpg"
+import FlatWhite    from "/src/assets/products/Flat White.jpg"
+
+const ALL_PRODUCTS = [
+    {
+        name: "Americano",
+        price: 7,
+        unit: "$",
+        src: Americano
+    },
+    {
+        name: "Cappucino",
+        price: 7.5,
+        unit: "$",
+        src: Cappuccino
+    },
+    {
+        name: "Latte",
+        price: 8,
+        unit: "$",
+        src: Latte
+    },
+    {
+        name: "FlatWhite",
+        price: 7.5,
+        unit: "$",
+        src: FlatWhite
+    },
+]
+
+export default ALL_PRODUCTS
